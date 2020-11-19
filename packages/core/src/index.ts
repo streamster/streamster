49e6aef8f31db178cbc0acb7025a1c6e07ea3116
Ben Tyler <ben@stae.co>
2020-11-19 07:23:13 -0800

    chore: stub functionality for core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,15 @@
-export const testingFlag = 'Here I am';
+export const validateKeys = (keys: string[], requiredKeys: string[]) => {
+  const errorCount = requiredKeys.reduce((acc: string[], curr: string) => {
+    if (!keys.includes(curr)) acc.push(curr);
+    return acc;
+  }, []);
+  if (errorCount.length > 0) {
+    return {
+      error: 'Missing required keys',
+      message: `The following required keys were missing: ${errorCount.join(
+        ', '
+      )}`,
+    };
+  }
+  return true;
+};
